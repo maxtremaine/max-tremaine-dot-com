153e86813028580dc57738666828815dec45d6ab
Max Tremaine <max.tremaine@gmail.com>
2021-01-09 20:07:19 -0500

    Setting the stage for a rebuild.

---
+++ construction.js
@@ -0,0 +1,12 @@
+/**
+ * Creates article items in the following format:
+ *     <tr class=[class] style="vertical-align: top;">
+ *         <td style="padding: 2vh;">
+ *             <a href=[link] target="_blank">[title]</a>
+ *         </td>
+ *         <td style="padding: 2vh;">[description]</td>
+ *     </tr>
+ */
+const generateArticleNode = ({title, description}) => {
+    
+}
